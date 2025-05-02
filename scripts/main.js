ea5b32b1fd2d2e67d5ff4584751b41bb65f1229e
pedroivo1 <pedroivoal1@gmail.com>
2025-05-01 21:29:02 -0300

    .

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const navLinks = document.querySelectorAll('header nav ul li a');
+const sections = document.querySelectorAll('main section, footer');
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    sections.forEach(section => {
+      section.setAttribute('hidden', 'true');
+    });
+    
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.removeAttribute('hidden');
+    }
+    
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+document.getElementById('sobre').removeAttribute('hidden');
