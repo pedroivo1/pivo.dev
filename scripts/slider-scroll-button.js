db53c7ebc6b73f23148b9aa8a442217581608211
pedroivo1 <pedroivoal1@gmail.com>
2025-05-21 23:10:46 -0300

    .

--- scripts/slider-scroll-button.js
+++ scripts/slider-scroll-button.js
@@ -4,25 +4,15 @@ const preBtn = [...document.querySelectorAll('.pre-btn')];
 
 const scrollC = 0.7;
 
-function getCardWidthWithMargin(container) {
-    const card = container.querySelector('.card');
-    const style = window.getComputedStyle(card);
-    const width = card.offsetWidth;
-    const marginRight = parseInt(style.marginRight) || 0;
-    return width + marginRight;
-}
-
 let containerWidths = productContainers.map(item => {
     const baseWidth = item.getBoundingClientRect().width * scrollC;
-    const cardWidth = getCardWidthWithMargin(item);
-    return baseWidth < cardWidth ? cardWidth : baseWidth;
+    return baseWidth;
 });
 
 window.addEventListener('resize', () => {
     containerWidths = productContainers.map(item => {
         const baseWidth = item.getBoundingClientRect().width * scrollC;
-        const cardWidth = getCardWidthWithMargin(item);
-        return baseWidth < cardWidth ? cardWidth : baseWidth;
+        return baseWidth;
     });
 });
 
